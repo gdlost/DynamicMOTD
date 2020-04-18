3480c753397c137599653af87ad5ce5d1e6115bd
Juan <gdl05x@gmail.com>
2020-04-18 16:29:54 -0400

    Timer: A BukkitTask executing a TimerAction
    constructor => plugin : communication with the plugin and server
                   seconds: when is executed the TimerAction
              seconds_copy: holds original seconds, for reset seconds
                            variable when is descreasing in the task.
    
    init => if action is null, throws an exception (useful, no?)
            starts the bukkit task, with a 20ticks period (1 second)
            when seconds == 0, then, the value of seconds is set to
            initial.
    
    TimerAction: a basic action executed by Timer, each x seconds.
                 if in timer isnt specified, then, exception is raised.

--- src/com/github/gdlost/dynamicmotd/Timer.java
+++ src/com/github/gdlost/dynamicmotd/Timer.java
@@ -1,12 +1,47 @@
 package com.github.gdlost.dynamicmotd;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scheduler.BukkitTask;
 
 public class Timer {
 
 	JavaPlugin plugin;
+	int seconds, seconds_copy;
 
-	public Timer(JavaPlugin plugin) {
+	BukkitTask thread;
+	TimerAction action;
+
+	public Timer(JavaPlugin plugin, int seconds) {
 		this.plugin = plugin;
+		this.seconds = seconds;
+		this.seconds_copy = seconds;
+	}
+
+	public final void init() throws TimerNullActionException {
+		if (action == null)
+		{
+			throw new TimerNullActionException("please call Timer.registerAction method before!");
+		}
+		plugin.getServer().broadcastMessage("Init timer");
+		thread = new BukkitRunnable() {
+
+			@Override
+			public void run() {
+				if (seconds-- <= 0) {
+					action.executeAction();
+					seconds = seconds_copy;
+				}
+			}
+		}.runTaskTimer(this.plugin, 0L, 20L);
+	}
+
+	public void registerAction(TimerAction action)
+	{
+		this.action = action;
+	}
+
+	public void stop() {
+		thread.cancel();
 	}
 }
---
+++ src/com/github/gdlost/dynamicmotd/TimerAction.java
@@ -0,0 +1,4 @@
+package com.github.gdlost.dynamicmotd;
+
+public interface TimerAction {
+}
