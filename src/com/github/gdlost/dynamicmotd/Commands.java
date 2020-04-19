a460f62b98e24124748f5b4c41d40c3866c3790f
Juan <gdl05x@gmail.com>
2020-04-18 22:10:51 -0400

    Commands for v1.0
    The dynamicmotd command accepts the following arguments:
    - help: Shows help.
    - reload/rl: Reloads plugin configuration file.
    - add <MTOD>: adds a MTOD to the configuration file,
                  and adds it to the current mtod list.
    - addt <MTOD>: adds a temporary MTOD to the
                  current mtod list.
    - list: Prints a list with all MTODs loaded (temporary included)
    - listt: Prints a list of all temporary mtods
    - listp: Prints a list of all mtods in the config file.
    
    If incorrect argument is provided, a message:
    "Please provide correct arguments" will appear in the
    sender chat.

--- src/com/github/gdlost/dynamicmotd/Commands.java
+++ src/com/github/gdlost/dynamicmotd/Commands.java
@@ -1,11 +1,13 @@
 package com.github.gdlost.dynamicmotd;
 
 import org.bukkit.ChatColor;
-import org.bukkit.Server;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.List;
+import java.util.StringJoiner;
 
 public class Commands implements CommandExecutor {
 	JavaPlugin plugin;
@@ -14,7 +16,7 @@ public class Commands implements CommandExecutor {
 	}
 	@Override
 	public boolean onCommand(CommandSender commandSender, Command command, String label, String[] args) {
-		if (label.equalsIgnoreCase("dynamicmotd")) {
+		if (label.equalsIgnoreCase("dynamicmotd") && commandSender.hasPermission("dynamicmotd.global")) {
 			switch (args.length) {
 				case 0:
 					commandSender.sendMessage(ChatColor.GOLD + "DynamicMotd 1.0, by gdlost");
@@ -37,39 +39,72 @@ public class Commands implements CommandExecutor {
 						commandSender.sendMessage(ChatColor.RED + "Usage: " + ChatColor.BLUE +
 								"/dynamicmotd add This is a new motd text");
 					}
+					else if (args[0].equalsIgnoreCase("list")) {
+						commandSender.sendMessage(ChatColor.GREEN + "DynamicMOTD - MOTD List:");
+						for (String s : ServerList.mlist.getCurrentMotdList()) {
+							commandSender.sendMessage("- " + ChatColor.translateAlternateColorCodes('&', s));
+						}
+					}
+					else if (args[0].equalsIgnoreCase("listt")) {
+
+						List<String> tempMotd = ServerList.mlist.getForNowMotdList();
+						if (tempMotd == null) {
+							commandSender.sendMessage(ChatColor.RED + "Temporary MOTDs list is empty");
+							return true;
+						}
+						commandSender.sendMessage(ChatColor.GREEN + "DynamicMOTD - MOTD List:");
+						for (String s : ServerList.mlist.getForNowMotdList()) {
+							commandSender.sendMessage("- " + ChatColor.translateAlternateColorCodes('&', s));
+						}
+					}
+					else if (args[0].equalsIgnoreCase("listp")) {
+						commandSender.sendMessage(ChatColor.GREEN + "DynamicMOTD - Saved MOTD List:");
+						for (String s : ServerList.mlist.getFileMotdList()) {
+							commandSender.sendMessage("- " + ChatColor.translateAlternateColorCodes('&', s));
+						}
+					}
 					return true;
 				default:
 					if (args[0].equalsIgnoreCase("add")
 						|| args[0].equalsIgnoreCase("addt")) {
-						StringBuilder stringBuilder = new StringBuilder();
+						StringJoiner motdString = new StringJoiner(" ");
 						for(int i = 1; i < args.length; i++)
 						{
-							stringBuilder.append(args[i]);
+							motdString.add(args[i]);
 						}
 						if (args[0].equalsIgnoreCase("add")) {
-							ServerList.mlist.addMotd(stringBuilder.toString());
+							ServerList.mlist.addMotd(motdString.toString());
 							commandSender.sendMessage(ChatColor.GREEN + "New motd added: " +
-									ChatColor.translateAlternateColorCodes('&', stringBuilder.toString()));
+									ChatColor.translateAlternateColorCodes('&', motdString.toString()));
 						}
 						else {
-							ServerList.mlist.addMotdForNow(stringBuilder.toString());
-							commandSender.sendMessage(ChatColor.GREEN + "New motd added: " +
-									ChatColor.translateAlternateColorCodes('&', stringBuilder.toString()));
-							commandSender.sendMessage(ChatColor.RED + "> Only available for this session! " +
-									"(or until the plugin reload)");
+							if (ServerList.mlist.addMotdForNow(motdString.toString())) {
+								commandSender.sendMessage(ChatColor.GREEN + "New motd added: " +
+										ChatColor.translateAlternateColorCodes('&', motdString.toString()));
+								commandSender.sendMessage(ChatColor.RED + "> Only available for this session! " +
+										"(or until the plugin reload)");
+							} else commandSender.sendMessage(ChatColor.RED +
+									"Error: You can't add more temporary motds, max reached");
 						}
 
 					}
+					else /* if no arguments specified */
+						commandSender.sendMessage(ChatColor.GOLD + "Please provide correct arguments. " +
+								"See /dynamicmotd help");
 					return true;
 			}
 		}
-		return false;
+			return  true;
+		}
 	}
 
 	private void showUserHelp(CommandSender s) {
 		s.sendMessage(ChatColor.GREEN + "DynamicMOTD arguments:");
 		s.sendMessage(__command("add <Insert MOTD here>", "Add a new MOTD to the list"));
 		s.sendMessage(__command("addt <Insert MOTD here>", "Add a new MOTD to the list temporary"));
+		s.sendMessage(__command("list", "List all MOTDs"));
+		s.sendMessage(__command("listt", "List all temporary MOTDs"));
+		s.sendMessage(__command("listp", "List all permanent MOTDs"));
 		s.sendMessage(__command("reload", "Reloads plugin configuration"));
 		s.sendMessage(__command("help", "Shows this help text"));
 	}
