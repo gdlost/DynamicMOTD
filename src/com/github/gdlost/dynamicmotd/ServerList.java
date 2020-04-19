a406af187edb0e9dfb5b99848b6894b0f11ef564
Juan <gdl05x@gmail.com>
2020-04-18 22:16:59 -0400

    static class access

--- src/com/github/gdlost/dynamicmotd/ServerList.java
+++ src/com/github/gdlost/dynamicmotd/ServerList.java
@@ -9,18 +9,29 @@ import java.util.Random;
 
 public class ServerList implements Listener {
 
-	MotdList mlist;
 	JavaPlugin plugin;
+
+	public static MotdList mlist;
 
 	/* Just, the dark side */
 	boolean dummy;
 	int dummyNumber;
 	Random dummyRandom = new Random();
+
 	public ServerList(JavaPlugin plugin) {
 		this.plugin = plugin;
-		this.dummy = plugin.getConfig().getBoolean("dummy");
-		this.dummyNumber = plugin.getConfig().getInt("dummyNumber");
-		this.mlist = new MotdList(this.plugin, this.plugin.getServer().getMotd());
+
+		/* dummy settings */
+		resetDummy();
+		/* static member, we need to access to execWhenReload() class method */
+		mlist = new MotdList(this.plugin, this.plugin.getServer().getMotd());
+	}
+
+	private void resetDummy()
+	{
+		dummy = plugin.getConfig().getBoolean("dummy");
+		dummyNumber = plugin.getConfig().getInt("dummyNumber");
+
 	}
 
 	@EventHandler
@@ -28,4 +39,5 @@ public class ServerList implements Listener {
 		if(dummy) e.setMaxPlayers(dummyRandom.nextInt(dummyNumber));
 		e.setMotd(mlist.getMotd());
 	}
+
 }
